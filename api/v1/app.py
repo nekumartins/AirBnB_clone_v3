f0f269c815ea8d2a96bc27f22811ec28548d220f
Oluwatunmibi Favour <ofavourite001@gmail.com>
2024-06-11 16:46:14 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Close storage on teardown"""
