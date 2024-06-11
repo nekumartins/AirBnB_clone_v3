728b55b8d73233b1ace8ad65895b41f29664f9cf
Favour <ofavourite001@gmail.com>
2024-06-11 17:49:58 +0100

    fix2

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Flask App that integrates with AirBnB static HTML Template
 """
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Flask route that returns json status response
 """
